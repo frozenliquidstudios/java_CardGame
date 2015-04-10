a999bd87a4603a7c0f440c6a79a9c402e8c9aa99
Jere Hupanen <hupanenjere@gmail.com>
2015-04-10 23:48:22 +0300

    Modified JavaDoc

--- cardBase/src/main/java/cardBase/CardDeck.java
+++ cardBase/src/main/java/cardBase/CardDeck.java
@@ -10,69 +10,105 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JApplet;
 
+/**
+ *
+ * @author LiQuid
+ */
 public class CardDeck extends JApplet{
     
     private ArrayList deck;
     private final String imageLocation = "src/main/java/cardImages/";
     private int cardCount; // Amount of cards in play.
     
-    public CardDeck() { // Create an empty deck.
+    /**
+     * Create an empty deck.
+     */
+    public CardDeck() {
         deck = new ArrayList();
         cardCount = 0;
     }
     
-    public void addCard(Card card) { // Add a card to the deck.
+    /**
+     *
+     * @param card Add card to the deck.
+     */
+    public void addCard(Card card) { 
         deck.add(card);
     }
     
-    public Card dealCard() {        // Deal a single card.
-      if(cardCount >= deck.size()) // If deck has no cards left, return null
-         return null;             // else get a card and increase cardCount.
+    /**
+     * @return If deck has no cards left, return null,
+     *         else get a card and increase cardCount.
+     */
+    public Card dealCard() {  // Deal a single card.
+      if(cardCount >= deck.size())  
+         return null;              
       else                           
          return (Card)deck.get(cardCount++);
     }
     
-    public int getDeckSize() {  // Get size of full deck.
+    /**
+     *
+     * @return Get size of full deck.
+     */
+    public int getDeckSize() {
         return deck.size();
     }
     
-    public int getRemainingCards() { // Get amount of cards left in the deck.
+    /**
+     *
+     * @return Get amount of cards left in the deck.
+     */
+    public int getRemainingCards() { 
         return deck.size() - cardCount;
     }
     
-    public boolean hasCards() { // Check if deck still contains cards.
+    /**
+     *
+     * @return Check if deck still contains cards.
+     */
+    public boolean hasCards() { 
         if(cardCount >= deck.size()) 
             return true;
         else 
             return false;
     }    
     
+    /**
+     * Fill deck with 52 cards, all unique.
+     */
     public void fillDeck() {
-        Iterator suitIterator = CardSuit.VALUES.iterator();
-		while(suitIterator.hasNext()) {
-		    CardSuit suit = (CardSuit) suitIterator.next();
-		    Iterator rankIterator = CardValue.VALUES.iterator();
+        Iterator suitIterator = CardSuit.VALUES.iterator(); // Iterates through all suits.
+		while(suitIterator.hasNext()) {            // Checks if more suits to cycle through.
+		    CardSuit suit = (CardSuit) suitIterator.next();  // Adds the suit to the card.
+		    Iterator valueIterator = CardValue.VALUES.iterator(); // Initializes value iterator.
                     
-		while(rankIterator.hasNext()) {
-		    CardValue value = (CardValue) rankIterator.next();	
-                    String imageFile = imageLocation + Card.getImageFilename(suit, value);         
-                    BufferedImage img = null;
+		while(valueIterator.hasNext()) {  // Checks if more values to cycle through.
+		    CardValue value = (CardValue) valueIterator.next();                       // Adds the value to the card.
+                    String imageFile = imageLocation + Card.getImageFilename(suit, value);   // Stores image filename.
+                    BufferedImage img = null; // Initializes image as null.
                     try {
-                        img = ImageIO.read(new File(imageFile));
+                        img = ImageIO.read(new File(imageFile));    // Places proper image path to img variable
                     }
                     catch (IOException e) {
-                        e.printStackTrace();  
+                        e.printStackTrace();  // Traces any errors
                     }   
-		    Card card = new Card(suit, value, img);
-		    addCard(card);
+		    Card card = new Card(suit, value, img); // Creates the card and places suit, value and image file to it.
+		    addCard(card); // Adds card to deck.
 		}
             }
     }
     
-    public void shuffleDeck() { // Shuffle the cards in the deck.
+    /**
+     * Shuffle the cards in the deck.
+     */
+    public void shuffleDeck() { 
         Collections.shuffle(deck);
     }
 
+    /**
+     * Resets card count of deck to 0.
+     */
     public void resetDeck() {
         cardCount = 0;
     }  
